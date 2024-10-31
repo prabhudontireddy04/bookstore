56c556da3e4834796d20223841c92e5bd66ab802
prabhudontireddy04 <141991674+prabhudontireddy04@users.noreply.github.com>
2024-10-31 21:29:54 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+export const PORT=5555;
+export const mongoDBURL = 'mongodb+srv://prabhudontireddy:prabhu@cluster0.y1jz5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {PORT,mongoDBURL} from "./config.js";
+import mongoose from 'mongoose';
+import {Book} from './models/bookmodels.js';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+const app=express();
+app.use(express.json());
+app.use(cors());
+app.get('/',(request,response)=>{
+  console.log(request);
+  return response.status(234).send('welcome to mern stack tutorial');
+});
+app.use('/books',booksRoute);
+mongoose
+.connect(mongoDBURL)
+.then(()=>{
+console.log('app connected to database');
+app.listen(PORT,()=>{
+  console.log(`App is listening to port:${PORT}`)
+});
+})
+.catch((error)=>{
+console.log(error);
+});
